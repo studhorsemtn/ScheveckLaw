dbab4dcdeded17ebd8570ec52a6d1a79f623a6e7
Bryon <studhorsemtn@gmail.com>
2017-05-10 15:52:24 -0600

    First Commit

---
+++ main.js
@@ -0,0 +1,22 @@
+$(function () {
+    //  Cache the window object
+    var $window = $(window);
+
+    //Parallax background effect
+    $('section[data-type="background"]').each(function () {
+        var $bgobj = $(this); //   Assigning the object
+        $(window).scroll(function() {
+
+            //  scroll the background at the var speed
+            //  The yPos is a negative value because we're scrolling it UP!!
+
+            var yPos = -($window.scrollTop()) / $bgobj.data('speed');
+
+            // Put together our final background position
+            var coords = '50%  ' + yPos + 'px';
+
+            //  Move the background
+            $bgobj.css({backgroundPosition: coords});
+        });
+    });
+});
