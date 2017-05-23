25f68a9fb3bc856cfacddfa2888323cfe17df53b
Bryon <studhorsemtn@gmail.com>
2017-05-23 12:50:13 -0600

    cleaned up php code

--- expungeForm.php
+++ expungeForm.php
@@ -3,9 +3,8 @@ function Validate()
     {
         // Check for empty fields
         if(empty($_POST['name'])            ||
-                empty($_POST['email'])      ||
-                empty($_POST['phone'])      ||
-                empty($_POST['message'])    ||
+                empty($_POST['email'])        ||
+                empty($_POST['message'])   ||
                 !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
 
                 return false;
@@ -19,13 +18,12 @@ function SendEmail($to = 'tri.developmentstudios@gmail.com')
         if(Validate() == true) {
                 $name           =   $_POST['name'];
                 $email_address  =   $_POST['email'];
-                $phone          =   $_POST['phone'];
                 $message        =   $_POST['message'];
 
                 // Create the email and send the message
                 $email_subject  =   "Website Contact Form:  $name";
                 $email_body     =   "You have received a new message from your website contact form.\n\n"."Here are the
-                details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+                details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
                 $headers        =   "From: ScheveckLaw.com\n";
                 $headers        .= "Reply-To: $email_address";
                 // Send true on successful send.
