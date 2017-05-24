8f5197aad4967cde6656393fc91015595c800a51
Bryon <studhorsemtn@gmail.com>
2017-05-24 11:24:46 -0600

    changed background for form and index updated bio

--- expungeForm.php
+++ expungeForm.php
@@ -3,6 +3,7 @@ function Validate()
     {
         // Check for empty fields
         if(empty($_POST['name'])            ||
+                empty($_POST['surname'])   ||
                 empty($_POST['email'])        ||
                 empty($_POST['message'])   ||
                 !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
@@ -23,26 +24,26 @@ function SendEmail($to = 'studhorsemtn@gmail.com')
 
                 // Create the email and send the message
                 $email_subject  =   "Website Contact Form:  $name $surname";
-                $email_body     =   "You have received a new message from your website contact form.\n\n"."Here are the
-                details:\n\nName: $name\n\nLast Name: $surname\n\nEmail: $email_address\n\nMessage:\n$message";
+                $email_body     =   "You have received a new message from your website contact form.\n\n"."Here are the details:
+                  \n\nName: $name\n\nLast Name: $surname\n\nEmail: $email_address\n\nMessage:\n$message";
                 $headers        =   "From: ScheveckLaw.com\n";
                 $headers        .= "Reply-To: $email_address";
                 // Send true on successful send.
                 // Send false if failed
-                return (mail($to,$email_subject,$email_body,$headers))? true: false;
+                return (mail($to,$email_subject,$email_body,$headers)) ? true: false;
             }
-        else
-            // Invalid inputs
-            return 'err';
+        // else
+        //     // Invalid inputs
+        //     return 'err';
     }
 
     // Apply function(s). You will get true, false, or err
     $send   =   SendEmail();
 
     // On return, you can echo any result
-    if($send == 'err')
-        echo 'Invalid Fields.';
-    elseif($send == false)
+    // if($send == 'err')
+    //     echo 'Invalid Fields.';
+    if($send == false)
         echo 'An Error Occurred.';
     else
         echo 'Email Sent Successfully! We will contact you within 72 hrs.';
