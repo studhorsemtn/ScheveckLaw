05fdaae4ff6f61a5ff6c8b75076cd36f1ebceffc
Bryon <studhorsemtn@gmail.com>
2017-08-21 18:03:09 -0600

    made slight syntactical changes and added a line to lyndons profile

--- expungeForm.php
+++ expungeForm.php
@@ -4,20 +4,21 @@ require 'PHPMailer/PHPMailerAutoload.php';
 $mail = new PHPMailer;
 
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+$mail->SMTPDebug = 2;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'studhorsemtn@gmail.com';                 // SMTP username
-$mail->Password = 'SonjaGirl0401';                           // SMTP password
+$mail->Username = 'schevecklaw@gmail.com';                 // SMTP username
+$mail->Password = 'Montana24';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
 function Validate()
     {
         // Check for empty fields
-        if(empty($_POST['name'])            ||                
+        if(empty($_POST['name'])            ||
+                empty($_POST['phone'])       ||
                 empty($_POST['email'])        ||
                 empty($_POST['message'])   ||
                 !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
@@ -28,13 +29,13 @@ function Validate()
             return true;
     }
 
-    $mail->setFrom(  'name', $auto = false);
-    $mail->addAddress('studhorsemtn@gmail.com');     // Add a recipient
+    $mail->setFrom(  'schevecklaw@gmail.com', $_POST['name']);
+    $mail->addAddress('schevecklaw@gmail.com');     // Add a recipient
 
     $mail->addReplyTo($_POST['email'], $_POST['name']);
 
     $mail->Subject = 'Expungement Request';
-    $mail->Body    = $_POST['message'];
+    $mail->Body    = $_POST['message']."\n\n".$_POST['name']."\n".$_POST['phone'];
 
 
     if(!$mail->send()) {
