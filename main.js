48cd6b91735539743ee7cef5b2610af6814299ef
Bryon <studhorsemtn@gmail.com>
2017-08-28 22:26:10 -0600

    commented out code for hover on dropdown as it caused the dropdown to stay open-undesireable

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ $(function () {
     //Parallax background effect
     $('section[data-type="background"]').each(function () {
         var $bgobj = $(this); //   Assigning the object
-        $(window).scroll(function() {
+        $(window).scroll(function () {
 
             //  scroll the background at the var speed
             //  The yPos is a negative value because we're scrolling it UP!!
@@ -16,14 +16,16 @@ $(function () {
             var coords = '50%  ' + yPos + 'px';
 
             //  Move the background
-            $bgobj.css({backgroundPosition: coords});
+            $bgobj.css({
+                backgroundPosition: coords
+            });
         });
     });
 });
 
-// toggle dropdown on mouse hover, click and tap events
-$('.dropdown').on('mouseenter mouseleave click tap', function(event) {
-    if (!$('.navbar-toggle').is(':visible')) {
-        $('.dropdown-toggle').dropdown('toggle');
-    }
-});
+// // toggle dropdown on mouse hover, click and tap events
+// $('.dropdown').on('mouseenter mouseleave click tap', function(event) {
+//     if (!$('.navbar-toggle').is(':visible')) {
+//         $('.dropdown-toggle').dropdown('toggle');
+//     }
+// });
